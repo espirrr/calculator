e42ef91a01c0551a49376d62a8383b2a2e46df06
Espir <e.ingasalo@gmail.com>
2022-08-27 18:38:29 -1000

    Added operator functions.

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let calculatedNumber = 0;
+
+// Operators
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
